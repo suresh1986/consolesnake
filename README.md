9e9b98547fe25d1baa13236f33deef361396a66d
suresh1986 <dev@vdeveloper.co.in>
2013-01-24 20:28:27 +0530

    Finally eclipse is integrated with git

---
+++ README.md
@@ -0,0 +1,6 @@
+consolesnake
+============
+
+This is a basic snake game found in old nokia mobile but made for console.
+This is made in C++.
+This is just for learning Git.
---
+++ snake.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+int main() {
+//	int x1, x2, x3, x4, x6, x7, x8, x9;
+//	x1 + x2 + x3 = 15;
+//	x4 + x5 + x6 = 15;
+//	x7 + x8 + x9 = 15;
+//	x1 + x4 + x7 = 15;
+//	x2 + x5 + x8 = 15;
+//	x3 + x6 + x9 = 15;
+//	x1 + x5 + x9 = 15;
+//	x3 + x5 + x7 = 15;
+//	x5 = 5;
+	Sleep( 1240 );
+	return 0;
+}
